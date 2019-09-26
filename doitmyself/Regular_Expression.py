9342710b97b45d103038990cd82f8c45fcacc0de
shinjune <47058441+shinjjune@users.noreply.github.com>
2019-09-26 20:23:04 +0900

    Create Regular_Expression.py

---
+++ doitmyself/Regular_Expression.py
@@ -0,0 +1,28 @@
+data="""
+park 800905-1049118
+kim 700905-1059119
+"""
+
+result = []
+for line in data.split("\n"):
+    word_result = []
+    for word in line.split(" "):
+        if len(word) == 14 and word[:6].isdigit() and word[7:].isdigit():
+            word = word[:6] + "-" + "*******"
+        word_result.append(word)
+    result.append(" ".join(word_result))
+print("\n".join(result))
+
+# 정규식
+
+import re # 정규표현식을 사용하기 위한 re 모듈
+
+data="""
+park 800905-1049118
+kim 700905-1059119
+"""
+
+pat = re.compile("(\d{6})[-]\d{7}")
+print(pat.sub("\g<1>-*******",data))
+
+
